b4277d3e92906502751646eed70eba9288d54fff
SIHYEON LEE <130329058+sleeee-dev@users.noreply.github.com>
2023-04-10 14:32:27 +0900

    [Bronze V] Title: 구구단, Time: 160 ms, Memory: 16200 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args)  throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        if(n>=1 && n<=9) {
+            for (int i = 1; i <= 9; i++) {
+                System.out.println(n + " * " + i + " = " + n * i);
+            }
+        }
+    }
+}

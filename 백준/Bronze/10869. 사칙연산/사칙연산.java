35f008fed1048a7bd9dd33a0edd6b96c78cb93f1
SIHYEON LEE <130329058+sleeee-dev@users.noreply.github.com>
2023-04-10 14:35:11 +0900

    [Bronze V] Title: 사칙연산, Time: 208 ms, Memory: 17768 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scan = new Scanner(System.in);
+        int A= scan.nextInt();
+        int B= scan.nextInt();
+
+        System.out.println(A+B);
+        System.out.println(A-B);
+        System.out.println(A*B);
+        System.out.println(A/B);
+        System.out.println(A%B);
+    }
+}

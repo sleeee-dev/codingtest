6b7488c44b359547e8eedb643d1a7ce2e7eb8a06
SIHYEON LEE <sihyeon.leeee@gmail.com>
2023-05-08 23:26:16 +0900

    [level 0] Title: 각도기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120829. 각도기/각도기.py
+++ 프로그래머스/lv0/120829. 각도기/각도기.py
@@ -1,11 +1,5 @@
 def solution(angle):
-    answer = 0
-    if 0 < angle < 90 :
-        answer = 1
-    elif 90 < angle < 180 :
-        answer = 3
-    elif angle == 90 :
-        answer = 2
-    elif angle == 180 :
-        answer = 4
-    return answer
+    if angle <= 90 :
+        return 1 if angle < 90 else 2
+    else :
+        return 3 if angle <180 else 4

3ffbd9dd1c9504288b24bbf45ccd989538d5865e
SIHYEON LEE <sihyeon.leeee@gmail.com>
2023-05-11 17:54:16 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120854. 배열 원소의 길이/배열 원소의 길이.py
@@ -0,0 +1,10 @@
+def solution(strlist):
+    return [len(i) for i in strlist]
+
+
+# 처음 풀이
+# def solution(strlist):
+#     answer = []
+#     for i in strlist:
+#         answer.append(len(i))
+#     return answer

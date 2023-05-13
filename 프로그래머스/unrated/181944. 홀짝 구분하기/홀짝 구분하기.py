95d970366e300458aab35ad2b88c6a6985d2bc3d
SIHYEON LEE <sihyeon.leeee@gmail.com>
2023-05-13 20:06:10 +0900

    [unrated] Title: 홀짝 구분하기, Time: 14.68 ms, Memory: 7.42 MB -BaekjoonHub

--- 프로그래머스/unrated/181944. 홀짝 구분하기/홀짝 구분하기.py
+++ 프로그래머스/unrated/181944. 홀짝 구분하기/홀짝 구분하기.py
@@ -1,5 +1,8 @@
 n = int(input())
-if n%2==0:
-    print(f'{n} is even')
-else:
-    print(f'{n} is odd')
+print(f'{n} is','even' if n%2==0 else 'odd')
+
+# 처음 풀이
+# if n%2==0:
+#     print(f'{n} is even')
+# else:
+#     print(f'{n} is odd')

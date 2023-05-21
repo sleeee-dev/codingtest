3a178f8add70323ef14ab9ece90e655664f84cfd
SIHYEON LEE <sihyeon.leeee@gmail.com>
2023-05-22 03:01:44 +0900

    [level 0] Title: 합성수 찾기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.py
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.py
@@ -1,7 +1,7 @@
 def solution(n):
     num = []
     answer = 0
-    for i in range(n+1):
+    for i in range(4,n+1):
         for j in range(1,i+1):
             if i%j==0 :
                 num.append(i)
@@ -14,6 +14,7 @@ def solution(n):
 # n이하의 합성수 찾기
 # num은 약수를 저장할 리스트
 # i는 약수, j는 약수를 찾기 위한 변수
+# 가장 작은 합성수가 4니까 4부터
 # 약수를 찾아서 num에 저장
 # count로 num에 저장된 약수가 3개 이상이면
 # answer(합성수 개수)+=1 해서 합성수 개수 반환

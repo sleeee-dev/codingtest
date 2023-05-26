601bd80d04c2547fcd802c4bd5a6996559efeae1
SIHYEON LEE <sihyeon.leeee@gmail.com>
2023-05-26 21:43:38 +0900

    [level 0] Title: 양꼬치, Time: 0.02 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120830. 양꼬치/양꼬치.java
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.java
@@ -1,17 +1,21 @@
 class Solution {
     public int solution(int n, int k) {
-        //양꼬치 n인분 , 음료수 k개
-        //=> 양꼬치 (n) 10인분당 음료수 +1 => k=k2+bonus
-        int answer = 0;
-        int bonus = 0;
-        int k2 = 0;
-        if(n>=10){
-            bonus=n/10;
-            k2=k-bonus;
-            answer=(12000*n)+(2000*k2);
-        }else{
-            answer=(12000*n)+(2000*k);
-        }
-        return answer;
+        return n * 12000 + k * 2000 - (n / 10 * 2000);
     }
 }
+
+
+//처음풀이
+        // //양꼬치 n인분 , 음료수 k개
+        // //=> 양꼬치 (n) 10인분당 음료수 +1 => k=k2+bonus
+        // int answer = 0;
+        // int bonus = 0;
+        // int k2 = 0;
+        // if(n>=10){
+        //     bonus=n/10;
+        //     k2=k-bonus;
+        //     answer=(12000*n)+(2000*k2);
+        // }else{
+        //     answer=(12000*n)+(2000*k);
+        // }
+        // return answer;

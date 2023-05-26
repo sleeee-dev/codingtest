a8f3c80d0899489f797643281b4594f8cd532e52
SIHYEON LEE <sihyeon.leeee@gmail.com>
2023-05-26 21:57:30 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 1.78 ms, Memory: 87.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String my_string, int n) {
+        String answer = "";
+        String[] a = my_string.split("");
+        for(int i=0;i<my_string.length();i++){
+            answer += a[i].repeat(n);
+        }
+        return answer;
+    }
+}

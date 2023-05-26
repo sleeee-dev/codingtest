225391135bff5a669576aec6f5006f4a79da66b4
SIHYEON LEE <sihyeon.leeee@gmail.com>
2023-05-26 21:36:54 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.01 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
+++ 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -1,9 +1,6 @@
 class Solution {
     public int solution(int num1, int num2) {
-        int answer = 0;
         double a = (double) num1 / num2 * 1000;
-
-        answer = (int) a;
-        return answer;
+        return (int) a;
     }
 }
